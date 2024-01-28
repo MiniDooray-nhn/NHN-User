ef3ace8369fafa9c39195fcab451969998d6a4a0
masiljangajji <hjk172262@gmail.com>
2024-01-28 19:21:46 +0900

    feat: valid User Request Valid 추가

--- src/main/java/com/nhnacademy/account/dto/UserModifyRequest.java
+++ src/main/java/com/nhnacademy/account/dto/UserModifyRequest.java
@@ -1,9 +1,13 @@
 package com.nhnacademy.account.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 
 @Getter
@@ -12,10 +16,19 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserModifyRequest {
 
+    @NotEmpty
+    @NotNull
+    @Length(max = 30)
     String password;
 
+    @Email
+    @NotNull
+    @Length(max = 50)
     String email;
 
+    @NotNull
+    @NotEmpty
+    @Length(max = 20)
     String name;
 
 
--- src/main/java/com/nhnacademy/account/dto/UserRegisterRequest.java
+++ src/main/java/com/nhnacademy/account/dto/UserRegisterRequest.java
@@ -1,9 +1,13 @@
 package com.nhnacademy.account.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 
 @Getter
@@ -12,12 +16,24 @@ import lombok.Setter;
 @AllArgsConstructor
 public class UserRegisterRequest {
 
+    @NotEmpty
+    @NotNull
+    @Length(max = 30)
     String id;
 
+    @NotEmpty
+    @NotNull
+    @Length(max = 30)
     String password;
 
+    @Email
+    @NotNull
+    @Length(max = 50)
     String email;
 
+    @NotNull
+    @NotEmpty
+    @Length(max = 20)
     String name;
 
 
